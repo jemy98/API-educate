3f14154a1ee00516d386f80151605fe0cdd93b5d
setoYWS <syws2898@gmail.com>
2023-06-22 13:03:30 +0700

    update createuser

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path =require('path')
 const connectDB = require('./config/dbConn')
 const mongoose = require('mongoose')
 const PORT = process.env.PORT || 3500;
+const bodyparser = require('body-parser')
 
 console.log(process.env.NODE_ENV)
 
@@ -13,7 +14,7 @@ connectDB()
 app.use('/',express.static(path.join(__dirname,'/public')))
 
 app.use('/',require('./routes/root'))
-app.use('/users', require('./routes/userRoutes'))
+app.use('/users', bodyparser.urlencoded(),require('./routes/userRoutes'))
 
 
 mongoose.connection.once('open', () => {
