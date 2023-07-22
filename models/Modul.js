9b3f36ab476e021568c7f94410b5985c93c4c786
jemy98 <sate85969@gmail.com>
2023-07-22 07:50:08 +0700

    description

--- models/Modul.js
+++ models/Modul.js
@@ -10,7 +10,7 @@ const modulSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    materi: {
+    description: {
         type: String,
         required: true
     },
